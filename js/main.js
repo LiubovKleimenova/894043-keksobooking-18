25f3cba628010a8c5431d44de58927a68480ea9a
LiubovKleimenova <kleimenovaliubov@gmail.com>
2019-10-03 23:43:27 -0400

    частично сделана карточка размещения

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ mapDialog.classList.remove('map--faded');
 var pinsContainer = mapDialog.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 
 // функция, для выбора случайного элемента из массива
 var getRandomElement = function (anyArray) {
@@ -58,7 +59,7 @@ var getRandomOffer = function () {
   return {
     title: getRandomElement(TITLES),
     address: getRandomFromRange(0, LOCATION_MAX) + ', ' + getRandomFromRange(0, LOCATION_MAX),
-    price: getRandomFromRange(0, PRICE_MAX) + ' $',
+    price: getRandomFromRange(0, PRICE_MAX),
     type: getRandomElement(TYPES),
     rooms: getRandomFromRange(ROOMS_MIN, ROOMS_MAX),
     guests: getRandomFromRange(GUESTS_MIN, GUESTS_MAX),
@@ -112,4 +113,48 @@ var renderPins = function (pins) {
   }
   return fragment;
 };
+
 pinsContainer.appendChild(renderPins(accomodationMocks));
+
+// перевод типов в наименования
+var transformTypeToHousingName = function (accomodationType) {
+  var housingName = '';
+  if (accomodationType = 'flat') {
+    housingName = 'Квартира';
+  }
+  if (accomodationType = 'bungalo') {
+    housingName = 'Бунгало';
+  }
+  if (accomodationType = 'house') {
+    housingName = 'Дом';
+  }
+  if (accomodationType = 'palace') {
+    housingName = 'Дворец';
+  }
+  return housingName;
+}
+
+// вывод фичей списком, проблемы где-то здесь или в строке 153
+var createFeaturesList = function (featuresArray, featureList) { // я пытаюсь создать функцию, которая принимает два параметра: массив фичей из моков и список ul из разметки (в строке 153)
+  for (var i = 0; i < featuresArray.length; i++) {
+    featureList.insertAdjacentHTML('afterbegin', '<li class="popup__feature popup__feature--' + featuresArray[i] + '></li>')
+  }
+  return featureList;
+}
+
+var renderCard = function (card) {
+  var cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = transformTypeToHousingName(card.offer.type);
+  cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+  createFeaturesList(card.offer.features, cardElement.querySelector('.popup__features'));
+  cardElement.querySelector('.popup__description').textContent = card.offer.description;
+
+  return cardElement;
+}
+
+console.log(renderCard(accomodationMocks[0]));
+console.log(renderCard(accomodationMocks[1]));
